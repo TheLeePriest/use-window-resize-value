d862af28780f37bab7eebed3dae59d4c108b77ca
Lee Priest <lpleepriest@gmail.com>
2020-04-19 13:38:45 +0100

    Updates

---
+++ precache-manifest.e140742f2e021fb472f2152c6691887a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f729bf16dbb8f6cc8fb6e1f265f40a3",
+    "url": "/use-window-resize-value/index.html"
+  },
+  {
+    "revision": "e2990f412e3f183f60d2",
+    "url": "/use-window-resize-value/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "84392bdf8435313c1877",
+    "url": "/use-window-resize-value/static/js/2.7ad67d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-window-resize-value/static/js/2.7ad67d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2990f412e3f183f60d2",
+    "url": "/use-window-resize-value/static/js/main.2f011752.chunk.js"
+  },
+  {
+    "revision": "68e39b960d8ad0917928",
+    "url": "/use-window-resize-value/static/js/runtime-main.7812fde4.js"
+  }
+]);
