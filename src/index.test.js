0141fcb9c9a2f303a533c7387d2d29b6c5715065
Lee Priest <lpleepriest@gmail.com>
2020-04-19 13:37:26 +0100

    Add some basic tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,53 @@
-import { useMyHook } from './'
-import { renderHook, act } from "@testing-library/react-hooks";
+import { useWindowResizeValue } from './index'
+import { renderHook, act } from '@testing-library/react-hooks'
 
-// mock timer using jest
-jest.useFakeTimers();
+const fireResizeEvent = width => {
+  global.window.innerWidth = width
+  global.window.dispatchEvent(new Event('resize'))
+}
 
-describe('useMyHook', () => {
-  it('updates every second', () => {
-    const { result } = renderHook(() => useMyHook());
+test('useWindowResizeValue should default to 1024 used by testing global', () => {
+  const { result } = renderHook(() => useWindowResizeValue())
 
-    expect(result.current).toBe(0);
+  expect(result.current).toBe(1024)
+})
 
-    // Fast-forward 1sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
+test('useWindowResizeValue should default to 1024 then be 1920 after resize event', () => {
+  const { result } = renderHook(() => useWindowResizeValue())
 
-    // Check after total 1 sec
-    expect(result.current).toBe(1);
+  expect(result.current).toBe(1024)
 
-    // Fast-forward 1 more sec
-    act(() => {
-      jest.advanceTimersByTime(1000);
-    });
-
-    // Check after total 2 sec
-    expect(result.current).toBe(2);
+  act(() => {
+    fireResizeEvent(1920)
   })
+
+  expect(result.current).toBe(1920)
+})
+
+test('useWindowResizeValue should be 500, then 1200, then 1920, then 500', () => {
+  const { result } = renderHook(() => useWindowResizeValue())
+
+  act(() => {
+    fireResizeEvent(500)
+  })
+
+  expect(result.current).toBe(500)
+
+  act(() => {
+    fireResizeEvent(1200)
+  })
+
+  expect(result.current).toBe(1200)
+
+  act(() => {
+    fireResizeEvent(1920)
+  })
+
+  expect(result.current).toBe(1920)
+
+  act(() => {
+    fireResizeEvent(500)
+  })
+
+  expect(result.current).toBe(500)
 })
